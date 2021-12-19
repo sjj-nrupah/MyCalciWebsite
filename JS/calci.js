6596b56bb8ee1b6a8b6d12fabbb9410fccbe040a
sjj-nrupah <85761514+sjj-nrupah@users.noreply.github.com>
2021-12-19 17:55:20 +0530

    Publishing everything to github

---
+++ JS/calci.js
@@ -0,0 +1,115 @@
+//JSON Object Creation
+var userip ={}
+
+//Reading the User Input Data for two values
+var readVal = (op) => {
+    userip.num1=document.querySelector("#num1").value;
+    userip.num2=document.querySelector("#num2").value;
+
+    if(userip.num1=="" || userip.num2==""){
+        alert("Enter both the values");
+        return;
+    };
+
+    userip.num1=parseInt(userip.num1);
+    userip.num2=parseInt(userip.num2);
+
+    switch(op) {
+        case add : add();
+        break;
+        case sub : sub();
+        break;
+        case multi : multi();
+        break;
+        case divide : divide();
+        break;
+        case quot : quot();
+        break;
+        case rem : rem();
+        break;
+    }
+}
+
+//Input Incomplete
+var inmsg = () => {
+    
+}
+
+//Addition
+var add = () => {
+    var num3=userip.num1+userip.num2;
+    document.querySelector("#ht").innerHTML = "The Sum is "+num3;
+    document.querySelector("#ht").style.visibility = "visible";
+}
+
+//Subtraction
+var sub = () => {
+    var num3=userip.num1-userip.num2;
+    document.querySelector("#ht").innerHTML = "The Difference is "+num3;
+    document.querySelector("#ht").style.visibility = "visible";
+}
+
+//Multiplication
+var multi = () => {
+    var num3=userip.num1*userip.num2;
+    document.querySelector("#ht").innerHTML = "The Product is "+num3;
+    document.querySelector("#ht").style.visibility = "visible";
+}
+
+//Division
+var divide = () => {
+    var num3=userip.num1/userip.num2;
+    document.querySelector("#ht").innerHTML = "The Division is "+num3;
+    document.querySelector("#ht").style.visibility = "visible";
+}
+
+//Quotient
+var quot = () => {
+    var num3=userip.num1/userip.num2;
+    num3=Math.floor(num3);
+    document.querySelector("#ht").innerHTML = "The Quotient is "+num3;
+    document.querySelector("#ht").style.visibility = "visible";
+}
+
+//Remainder
+var rem = () => {    
+    var num3=userip.num1%userip.num2;
+    document.querySelector("#ht").innerHTML = "The Remainder is "+num3;
+    document.querySelector("#ht").style.visibility = "visible";
+}
+
+//Single Value Calculations
+function oneval() {
+    document.querySelector("#num2").style.display="none";
+    document.querySelector("#lab2").style.display="none";
+    document.querySelector("#num1").style.position="relative";
+    document.querySelector("#num1").style.top="20px";
+    document.querySelector("#lab1").style.position="relative";
+    document.querySelector("#lab1").style.top="15px";
+    document.querySelector("#lab1").innerText="Enter the value : ";
+    document.querySelector("#lab1").style.fontSize="25px";
+    document.querySelector("#onevalue").style.display="none";
+    userip.num2=0;
+    console.log(userip.num2);
+}
+
+//Multiple Value Calculations
+var multival = () => {
+    var count=prompt("How many values you want to enter ? (3/4/5)");
+}
+
+//Clear Values Button
+var clearval = () => {
+    //alert("Sorry ! Temporarily Unavailable Feature");
+    document.querySelector("#num1").value="";
+    document.querySelector("#num2").value="";
+}
+
+//Clear Result Button
+var clearResult = () => {
+    document.querySelector("#ht").innerHTML = "";
+}
+
+/*var myDate = new Date();
+myDate=myDate.getDate();
+document.querySelector("#Nrupah").innerHTML = "Date : "+myDate;*/
